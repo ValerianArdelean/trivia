93428cff9e9fc5cc6fa74ab6df47936b0d293b19
ValerianArdelean <valerian_ardelean@yahoo.com>
2020-11-26 23:11:15 +0200

    feat: implement all table clases adding properties

--- models.py
+++ models.py
@@ -1,17 +1,90 @@
+# models module for Trivia app
 from flask_sqlalchemy import SQLAlchemy
 
 
+'''initiate SQLAlchemy to be used for clases declarations'''
+
+
 db = SQLAlchemy()
+
+
+'''app-db configurations need it to be in a function,
+   so app parameter can be used as atribute in 'app' module'''
+
+
 def setup_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://valerian@localhost:5432/trivia'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db = SQLAlchemy(app)
 
 
+'''declaring db scheema'''
+
+
 class Questions(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    question = db.Column(db.String(),nullable=False)
+    question = db.Column(db.String(), nullable=False)
+    answer = db.Column(db.String())
     difficulty = db.Column(db.Integer)
 
-    def __repr__(self):
-        return f'{self.id} {self.question} {self.difficulty}'
+    # initialize in memory the table
+    def __init__(self, id, question, answer, category, difficulty):
+        self.id = id
+        self.question = question
+        self.answer = answer
+        self.category = category
+        self.difficulty = difficulty
+
+    # creating attributes for db objects
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def sesion_close(self):
+        db.session.close()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'question': self.question,
+            'answer': self.answer,
+            'difficulty': self.difficulty
+        }
+
+
+class Categories(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(), nullable=False)
+
+    # initialize in memory the table
+    def __init__(self, id, type):
+        self.id = id
+        self.type = type
+
+    # creating attributes for db objects
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def sesion_close(self):
+        db.session.close()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'type': self.type
+        }
