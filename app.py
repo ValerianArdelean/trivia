2ecd82fd5258e4c3833bf53933cd0217acf77096
ValerianArdelean <valerian_ardelean@yahoo.com>
2020-11-26 20:05:54 +0200

    eat:Implement db conection
    Implement data-base conection and data base retrieve on home route

--- app.py
+++ app.py
@@ -3,11 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALACHEMY_DATABASE_URI'] = 'postgresql://valerian@localhost:5432/trivia'
-app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://valerian@localhost:5432/trivia'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+
+class Questions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(),nullable=False)
+    difficulty = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'{self.id} {self.question} {self.difficulty}'
 
 
 @app.route('/')
 def index():
-    return jsonify({'message':'maaa'})
+    questions = str(Questions.query.all())
+    return jsonify({'questions': questions})
